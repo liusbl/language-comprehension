7f21ce779310e6190a71732a1133865c4c687b39
Liudvikas Sablauskas <liudvikas.sabl@gmail.com>
2024-10-20 23:46:50 +0300

    Add all english words

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,27 +5,25 @@ import java.nio.file.Files
 import kotlin.io.path.Path
 
 /**
+ * Lithuanian:
+ * Source: https://ekalba.lt/bendrines-lietuviu-kalbos-zodynas
  * Total sum (including first caps words): 71449
  * Without first caps words: 68485
  * Without duplicates: 67163
- * Without hyphens: 67091
+ * [USING THIS] Without hyphens: 67091
+ *
+ * English:
+ * Source: https://github.com/dwyl/english-words?tab=readme-ov-file
+ * [USING THIS] Sum (without caps words or non-letters): 341122
  */
 fun main() {
     val gson = Gson()
-    val resultFile = File("lithuanian-words.txt")
+    val resultFile = File("english-words.txt")
     resultFile.delete()
-    Files.walk(Path("letters")).toList().drop(1).forEach { path ->
-        val file = path.toFile()
-        println(file)
-        val text = file.readText()
-        val json = gson.fromJson(text, JsonObject::class.java)
-        val list = json["details"].asJsonObject["list"].asJsonArray
-
-        val wordText = list.map { it.asJsonObject["header"].asString }
-            .filter { word -> word[0].isLowerCase() }
-            .filter { word -> !word.contains("-") }
-            .distinct()
-            .joinToString(separator = "\n")
-        resultFile.appendText(wordText)
-    }
+    val result = File("letters/words.txt").readLines()
+        .filter { word -> !word.contains("-") && !word.contains(".") }
+        .filter { it.all { it.isLowerCase() } }
+        .distinct()
+        .joinToString(separator = "\n")
+    resultFile.writeText(result)
 }
