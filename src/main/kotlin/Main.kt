adfa005880575d96661fc4193b1574f73342f54f
Liudvikas Sablauskas <liudvikas.sabl@gmail.com>
2024-10-21 00:51:31 +0300

    Word guessing game

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,4 @@
-import com.google.gson.Gson
-import com.google.gson.JsonObject
 import java.io.File
-import java.nio.file.Files
-import kotlin.io.path.Path
 
 /**
  * Lithuanian:
@@ -17,13 +13,66 @@ import kotlin.io.path.Path
  * [USING THIS] Sum (without caps words or non-letters): 341122
  */
 fun main() {
-    val gson = Gson()
-    val resultFile = File("english-words.txt")
-    resultFile.delete()
-    val result = File("letters/words.txt").readLines()
-        .filter { word -> !word.contains("-") && !word.contains(".") }
-        .filter { it.all { it.isLowerCase() } }
-        .distinct()
-        .joinToString(separator = "\n")
-    resultFile.writeText(result)
+    val resultFile = File("english-words-all-results.txt")
+    if (!resultFile.exists()) {
+        resultFile.createNewFile()
+    }
+    val wordResultList = resultFile.readLines().map(::WordResult)
+    val wordList = File("english-words-all.txt").readLines()
+    val shuffledWordList = wordList.filter { word -> !wordResultList.map { it.word }.contains(word) }
+        .shuffled()
+
+    println("Hello!")
+    val comprehensionText = Comprehension.entries.joinToString(separator = ", ") { comprehension ->
+        "${comprehension.text}: ${comprehension.shortcut}"
+    }
+    var currentSessionWords = 0
+    shuffledWordList.take(1000).forEach { word ->
+        println("----------------------------")
+        println(
+            "Analysed word count: ${wordResultList.size + currentSessionWords}/${wordList.size}, " +
+                    "${(wordResultList.size + currentSessionWords) / wordList.size.toFloat()}%"
+        )
+        println("Next word: $word")
+        println(comprehensionText)
+        val input = readln()
+        val comprehension = Comprehension(input[0])
+
+        if (comprehension == null) {
+            println("Error encountered. Exiting, press Enter")
+            readln()
+            return
+        } else {
+            resultFile.appendText("$word;${comprehension.percent}\n")
+            currentSessionWords++
+        }
+    }
+}
+
+data class WordResult(
+    val word: String,
+    val comprehension: Comprehension
+)
+
+fun WordResult(line: String): WordResult {
+    val (word, percent) = line.split(';')
+    return WordResult(word, Comprehension(percent.toInt())!!)
+}
+
+enum class Comprehension(
+    val text: String,
+    val shortcut: Char,
+    val percent: Int
+) {
+    Unheard("Unheard", 'U', 0),
+    HeardButUnknown("HeardButUnknown", 'H', 25),
+    Doubtful("Doubtful", 'D', 50),
+    Passable("Passable", 'P', 75),
+    Mastered("Mastered", 'M', 100);
+}
+
+fun Comprehension(percent: Int): Comprehension? = Comprehension.entries.find { it.percent == percent }
+
+fun Comprehension(shortcut: Char): Comprehension? = Comprehension.entries.find {
+    it.shortcut.equals(shortcut, ignoreCase = true)
 }
